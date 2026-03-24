4d25090810a02903b34bc69560b88e4c4153395b
Hex48 <hexual48@gmail.com>
2026-03-24 23:51:19 +0900

    [Bronze II] Title: 음계, Time: 48 ms, Memory: 5364 KB -BaekjoonHub

---
+++ C#/백준/Bronze/2920. 음계/음계.cs
@@ -0,0 +1,13 @@
+string n = Console.ReadLine();
+if (n == "1 2 3 4 5 6 7 8")
+{
+    Console.Write("ascending");
+}
+else if (n == "8 7 6 5 4 3 2 1")
+{
+    Console.Write("descending");
+}
+else
+{
+    Console.Write("mixed");
+}
