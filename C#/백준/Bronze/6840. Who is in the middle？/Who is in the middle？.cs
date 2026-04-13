314eb780a8c07660bbb00ef8b8c019efd52bbe51
Hex48 <hexual48@gmail.com>
2026-04-13 23:32:49 +0900

    [Bronze V] Title: Who is in the middle?, Time: 48 ms, Memory: 5532 KB -BaekjoonHub

---
+++ C#/백준/Bronze/6840. Who is in the middle？/Who is in the middle？.cs
@@ -0,0 +1,3 @@
+            int[] numbers = { int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()) };
+            Array.Sort(numbers);
+            Console.Write(numbers[1]);
