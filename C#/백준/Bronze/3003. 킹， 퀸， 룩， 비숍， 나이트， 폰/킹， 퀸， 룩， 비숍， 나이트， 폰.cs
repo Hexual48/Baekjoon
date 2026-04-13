eb3088fe4110f3a381379b3091c828974314bdec
Hex48 <hexual48@gmail.com>
2026-04-13 22:55:37 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 48 ms, Memory: 5512 KB -BaekjoonHub

---
+++ C#/백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cs
@@ -0,0 +1,7 @@
+            int[] array = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            Console.WriteLine(1 - array[0]);
+            Console.WriteLine(1 - array[1]);
+            Console.WriteLine(2 - array[2]);
+            Console.WriteLine(2 - array[3]);
+            Console.WriteLine(2 - array[4]);
+            Console.WriteLine(8 - array[5]);
