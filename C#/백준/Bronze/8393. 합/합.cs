5cb6ce72854294709fbdd190c2d050a31de3251b
Hex48 <hexual48@gmail.com>
2026-04-13 23:40:36 +0900

    [Bronze V] Title: 합, Time: 48 ms, Memory: 5496 KB -BaekjoonHub

---
+++ C#/백준/Bronze/8393. 합/합.cs
@@ -0,0 +1,7 @@
+            int a = int.Parse(Console.ReadLine());
+            int hap = 0;
+            for (int i = 0; i <= a; i++)
+            {
+                hap += i;
+            }
+            Console.WriteLine(hap);
